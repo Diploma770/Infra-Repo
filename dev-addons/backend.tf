ddc7352ddc710dcc3031d65dbfd1beef237c9c3c
Harutyun Grigoryan <harutyun.grigoryan897@gmail.com>
2026-03-05 23:03:20 +0400

    change veriables

--- dev-addons/backend.tf
+++ dev-addons/backend.tf
@@ -2,7 +2,6 @@ terraform {
   # backend "local" {
   #   path = "state/dev-addons/terraform.tfstate"
   # }
-  
   backend "gcs" {
     bucket = "my-dev-770-tfstate-dev"
     prefix = "terraform/dev-addons"
